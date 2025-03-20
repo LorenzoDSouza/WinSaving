eae0e814cdc7ca722e4b9714ecb74befba1f2cb7
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-20 17:50:28 -0300

    feat: create the expense table creation sql.

---
+++ api/src/main/resources/db/migration/V4__create-expenses-table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS expenses (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    value DOUBLE PRECISION NOT NULL,
+    expense_type VARCHAR(20) NOT NULL CHECK (expense_type IN (
+        'MERCADO', 'ALIMENTACAO', 'TRANSPORTE', 'HABITACAO', 'SAUDE',
+        'EDUCACAO', 'LAZER', 'COMPRAS', 'IMPOSTOS', 'OUTROS'
+    )),
+    description VARCHAR(255),
+    monthly_budget_id UUID NOT NULL,
+    FOREIGN KEY (monthly_budget_id) REFERENCES monthly_budgets(id) ON DELETE RESTRICT
+);
+
