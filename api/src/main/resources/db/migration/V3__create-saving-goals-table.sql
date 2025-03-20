0dcc5baf28a1c02298f154ab7ea2ace076fe4e4e
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-20 17:50:22 -0300

    feat: create the saving goal table creation sql.

---
+++ api/src/main/resources/db/migration/V3__create-saving-goals-table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS saving_goals (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    purpose VARCHAR(255),
+    deposit_place VARCHAR(255),
+    due_date DATE,
+    goal_amount NUMERIC(15, 2),
+    total_amount NUMERIC(15, 2) DEFAULT 0.00,
+    user_id UUID,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE NO ACTION
+);
