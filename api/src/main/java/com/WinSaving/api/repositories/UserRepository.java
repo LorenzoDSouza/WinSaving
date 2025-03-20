31ea3e334b56dceeb832f3db374353aeeea6bb89
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-20 10:55:06 -0300

    fix: fix the User import in the UserRepository file.

--- api/src/main/java/com/WinSaving/api/repositories/UserRepository.java
+++ api/src/main/java/com/WinSaving/api/repositories/UserRepository.java
@@ -1,6 +1,6 @@
 package com.WinSaving.api.repositories;
 
-import com.WinSaving.api.domain.User;
+import com.WinSaving.api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
