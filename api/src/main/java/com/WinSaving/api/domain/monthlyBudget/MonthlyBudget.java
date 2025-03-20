faf19efccd21de2d1b58dbf4dc7b7dcb85fcb73a
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-20 17:48:31 -0300

    fix: change the name of the table reference to monthly_budgets, change the logic for the values, using  a usedAmount atb and originalAmount (in the app, always keep the original value and decrement the used), added the payDay atb (int) that makes a reference of what day the usedAmount reset. Also, create the lasReset field, a date field that will be used to see if an expense that was deleted, will affect the usedAmount or not.

--- api/src/main/java/com/WinSaving/api/domain/monthlyBudget/MonthlyBudget.java
+++ api/src/main/java/com/WinSaving/api/domain/monthlyBudget/MonthlyBudget.java
@@ -7,27 +7,34 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Table(name = "monthly_budget")
+@Table(name = "monthly_budgets")
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class MonthlyBudget {
-
     @Id
     @GeneratedValue
     private UUID id;
 
-    private Double amount;
+    @Column(nullable = false, precision = 10, scale = 2)
+    private Double usedAmount = 0.00;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private Double originalAmount;
+
+    private int payDay;
+
+    private Date lastReset;
 
     @OneToMany(mappedBy = "monthlyBudget", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Expense> expenses = new ArrayList<>();
-
-
 }
+
 
