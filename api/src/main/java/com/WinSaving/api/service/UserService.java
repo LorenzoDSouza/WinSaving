891b41f2d5063aa97597981d2dae0d89984b7a10
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-24 20:19:51 -0300

    refactor: remove the logic of the validation.
    Now the logic is in an object validation class.

--- api/src/main/java/com/WinSaving/api/service/UserService.java
+++ api/src/main/java/com/WinSaving/api/service/UserService.java
@@ -1,10 +1,11 @@
 package com.WinSaving.api.service;
 
+import com.WinSaving.api.domain.monthlyBudget.MonthlyBudget;
 import com.WinSaving.api.domain.user.User;
 import com.WinSaving.api.domain.user.UserRequestDTO;
 import com.WinSaving.api.domain.user.UserResponseDTO;
 import com.WinSaving.api.repositories.UserRepository;
-import com.WinSaving.api.util.validation.*;
+import com.WinSaving.api.util.objectsValidation.UserRequestDTOValidation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,39 +16,19 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-    private final Validator<String> emailValidator;
-    private final Validator<String> phoneNumberValidator;
-    private final Validator<String> firstNameValidator;
-    private final Validator<String> lastNameValidator;
-    private final Validator<String> passwordValidator;
+    private final UserRequestDTOValidation userRequestDTOValidation;
 
 
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-        emailValidator = new EmailValidator();
-        phoneNumberValidator = new PhoneNumberValidator();
-        firstNameValidator = new FirstNameValidator();
-        lastNameValidator = new LastNameValidator();
-        passwordValidator = new PasswordValidator();
+        userRequestDTOValidation = new UserRequestDTOValidation();
     }
 
     @Transactional
     public UserResponseDTO createUser(UserRequestDTO dto) {
-        if (!emailValidator.validate(dto.email())){
-            throw new RuntimeException("Invalid email address!");
-        }
-        if (!phoneNumberValidator.validate(dto.phoneNumber())){
-            throw new RuntimeException("Invalid phone number!");
-        }
-        if (!firstNameValidator.validate(dto.firstName())){
-            throw new RuntimeException("Invalid first name!");
-        }
-        if (!lastNameValidator.validate(dto.lastName())){
-            throw new RuntimeException("Invalid last name!");
-        }
-        if (!passwordValidator.validate(dto.password())){
-            throw new RuntimeException("Invalid password!");
+        if(!userRequestDTOValidation.validate(dto)){
+            throw new RuntimeException("There was a problem while creating the user!");
         }
 
         User user = new User();
@@ -58,7 +39,7 @@ public class UserService {
         user.setLastName(dto.lastName());
         user.setPassword(passwordEncoder.encode(dto.password()));
 
-        //TODO: create the monthly budget to set to the user.
+        user.setMonthlyBudget(new MonthlyBudget());
 
         try {
             user = userRepository.save(user);
