9efb5df13c136874ab92ecbe3174762bd40a174d
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-24 17:32:35 -0300

    feat: implements the logic of the phone number validation class.

--- api/src/main/java/com/WinSaving/api/util/validation/PhoneNumberValidator.java
+++ api/src/main/java/com/WinSaving/api/util/validation/PhoneNumberValidator.java
@@ -1,4 +1,19 @@
 package com.WinSaving.api.util.validation;
 
-public class PhoneNumberValidator {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PhoneNumberValidator implements Validator<String> {
+
+    private static final Pattern phoneNumberPattern =  Pattern.compile(
+           "\\(?\\d{2}\\)?\\s?9\\d{4}-?\\d{4}|\\d{10}");
+
+    @Override
+    public boolean validate(String phoneNumber){
+        Matcher matcher = phoneNumberPattern.matcher(phoneNumber);
+
+        return matcher.matches();
+    }
+
+
 }
