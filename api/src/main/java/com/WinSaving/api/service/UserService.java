d6b39bc6133644e0c277dbc58068bf8f6357eace
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-24 18:14:21 -0300

    feat: add the last name validator to the user service creation method.

--- api/src/main/java/com/WinSaving/api/service/UserService.java
+++ api/src/main/java/com/WinSaving/api/service/UserService.java
@@ -4,7 +4,8 @@ import com.WinSaving.api.domain.user.User;
 import com.WinSaving.api.domain.user.UserRequestDTO;
 import com.WinSaving.api.domain.user.UserResponseDTO;
 import com.WinSaving.api.repositories.UserRepository;
-import com.WinSaving.api.util.validation.Validator;
+import com.WinSaving.api.util.validation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,15 +14,34 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final Validator<String> emailValidator;
+    private final Validator<String> phoneNumberValidator;
+    private final Validator<String> firstNameValidator;
+    private final Validator<String> lastNameValidator;
 
-    public UserService(UserRepository userRepository, Validator<String> emailValidator) {
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.emailValidator = emailValidator;
+        emailValidator = new EmailValidator();
+        phoneNumberValidator = new PhoneNumberValidator();
+        firstNameValidator = new FirstNameValidator();
+        lastNameValidator = new LastNameValidator();
     }
 
     @Transactional
     public UserResponseDTO createUser(UserRequestDTO dto) {
-        User user = new User();
+        if (!emailValidator.validate(dto.email())){
+            throw new RuntimeException("Invalid email address!");
+        }
+        if (!phoneNumberValidator.validate(dto.phoneNumber())){
+            throw new RuntimeException("Invalid phone number!");
+        }
+        if (!firstNameValidator.validate(dto.firstName())){
+            throw new RuntimeException("Invalid first name!");
+        }
+        if (!lastNameValidator.validate(dto.lastName())){
+            throw new RuntimeException("Invalid last name!");
+        }
 
         return null;
     }
