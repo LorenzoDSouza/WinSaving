ee4b60f364c5707efda5cc2a052b76bfbb94d28d
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-24 18:04:36 -0300

    feat: create the first name validator class (validation interface).

---
+++ api/src/main/java/com/WinSaving/api/util/validation/FirstNameValidator.java
@@ -0,0 +1,19 @@
+package com.WinSaving.api.util.validation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class FirstNameValidator implements Validator<String> {
+
+    public static final Pattern firstNamePattern =
+            Pattern.compile("^[A-Za-z]+$");
+
+    @Override
+    public boolean validate(String input) {
+        Matcher matcher = firstNamePattern.matcher(input);
+
+        return matcher.matches();
+    }
+
+}
