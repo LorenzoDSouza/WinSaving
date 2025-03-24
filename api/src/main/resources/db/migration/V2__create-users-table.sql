217e3699399391eb217d5b4d94d601563fc3af4c
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-24 16:07:48 -0300

    fix: modify the SQL for flyway, removing the name column and adding the first and last name column.

--- api/src/main/resources/db/migration/V2__create-users-table.sql
+++ api/src/main/resources/db/migration/V2__create-users-table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     phone_number VARCHAR(20) NOT NULL,
