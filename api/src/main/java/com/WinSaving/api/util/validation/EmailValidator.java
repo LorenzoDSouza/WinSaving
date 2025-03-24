87bcd2336170c2cc96b4a8a068fa7f090cf19283
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-24 17:06:41 -0300

    feat: create a class to validate user emails.
    The email validator is and implementation of the Validator interface.

---
+++ api/src/main/java/com/WinSaving/api/util/validation/EmailValidator.java
@@ -0,0 +1,18 @@
+package com.WinSaving.api.util.validation;
+
+import com.WinSaving.api.util.validation.Validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidator implements Validator<String> {
+    public static final Pattern emailPattern =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    @Override
+    public boolean validate(String email) {
+        Matcher matcher = emailPattern.matcher(email);
+
+        return matcher.matches();
+    }
+}
