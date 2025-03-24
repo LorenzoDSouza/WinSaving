63c25029683ca9cfb56670a5bb0ec4c81e2b9285
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-24 19:17:44 -0300

    feat: add the logic to create the user in the userService.
    Now, it's needed to be created the monthly budget inside the createUser method.

--- api/src/main/java/com/WinSaving/api/service/UserService.java
+++ api/src/main/java/com/WinSaving/api/service/UserService.java
@@ -6,6 +6,7 @@ import com.WinSaving.api.domain.user.UserResponseDTO;
 import com.WinSaving.api.repositories.UserRepository;
 import com.WinSaving.api.util.validation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     private final Validator<String> emailValidator;
     private final Validator<String> phoneNumberValidator;
     private final Validator<String> firstNameValidator;
@@ -48,8 +50,22 @@ public class UserService {
             throw new RuntimeException("Invalid password!");
         }
 
+        User user = new User();
 
-        return null;
+        user.setEmail(dto.email());
+        user.setPhoneNumber(dto.phoneNumber());
+        user.setFirstName(dto.firstName());
+        user.setLastName(dto.lastName());
+        user.setPassword(passwordEncoder.encode(dto.password()));
+
+        //TODO: create the monthly budget to set to the user.
+
+        try {
+            user = userRepository.save(user);
+            return new UserResponseDTO(user.getId(), user.getFirstName(), user.getLastName(), user.getEmail(), user.getPhoneNumber());
+        } catch (Exception e) {
+            throw new RuntimeException("Error while saving user: " + e.getMessage());
+        }
     }
 
 }
