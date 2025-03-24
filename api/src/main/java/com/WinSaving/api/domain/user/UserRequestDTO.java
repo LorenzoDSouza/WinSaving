ad400b2d9f19851940833e9ef04adb55da006f92
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-24 16:01:17 -0300

    fix: remove the empty monthly budget DTO from user's response and request DTO.

--- api/src/main/java/com/WinSaving/api/domain/user/UserRequestDTO.java
+++ api/src/main/java/com/WinSaving/api/domain/user/UserRequestDTO.java
@@ -1,12 +1,9 @@
 package com.WinSaving.api.domain.user;
 
-import com.WinSaving.api.domain.monthlyBudget.MonthlyBudgetDTO;
-
 public record UserRequestDTO(
         String name,
         String email,
         String password,
-        String phoneNumber,
-        MonthlyBudgetDTO monthlyBudget
+        String phoneNumber
 ) {
 }
--- api/src/main/java/com/WinSaving/api/domain/user/UserResponseDTO.java
+++ api/src/main/java/com/WinSaving/api/domain/user/UserResponseDTO.java
@@ -1,13 +1,10 @@
 package com.WinSaving.api.domain.user;
 
-import com.WinSaving.api.domain.monthlyBudget.MonthlyBudgetDTO;
-
 import java.util.UUID;
 
 public record UserResponseDTO(
         UUID id,
         String name,
         String email,
-        String phoneNumber,
-        MonthlyBudgetDTO monthlyBudget
+        String phoneNumber
 ) {}
