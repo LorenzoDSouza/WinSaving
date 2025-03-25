a16d413eb2e832d4dc9fb033d5f28d524c45b97f
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-25 16:39:44 -0300

    fix: remove the S3 instance method in the AWS config class.

--- api/src/main/java/com/WinSaving/api/config/AWSConfig.java
+++ api/src/main/java/com/WinSaving/api/config/AWSConfig.java
@@ -1,8 +1,6 @@
 package com.WinSaving.api.config;
 
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,11 +11,4 @@ public class AWSConfig {
     @Value("${aws.region}")
     private String awsRegion;
 
-
-    @Bean
-    public AmazonS3 createS3Instance() {
-        return AmazonS3ClientBuilder.standard()
-                .withRegion(awsRegion)
-                .build();
-    }
 }
