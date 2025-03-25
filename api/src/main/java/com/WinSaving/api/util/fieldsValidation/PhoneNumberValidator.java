6aad1aa57f9b3e0e366de5af3ee1428a5ba0334a
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-25 12:42:34 -0300

    feat + refactor: create a logic in the phone number validator, that checks if it's empty or null, unique and the pattern is valid.

--- api/src/main/java/com/WinSaving/api/util/fieldsValidation/PhoneNumberValidator.java
+++ api/src/main/java/com/WinSaving/api/util/fieldsValidation/PhoneNumberValidator.java
@@ -1,19 +1,43 @@
 package com.WinSaving.api.util.fieldsValidation;
 
-import java.util.regex.Matcher;
+import com.WinSaving.api.repositories.UserRepository;
+import org.springframework.stereotype.Component;
+
 import java.util.regex.Pattern;
 
+@Component
 public class PhoneNumberValidator implements Validator<String> {
-
+    private final UserRepository userRepository;
     private static final Pattern phoneNumberPattern =  Pattern.compile(
            "\\(?\\d{2}\\)?\\s?9\\d{4}-?\\d{4}|\\d{10}");
 
-    @Override
-    public boolean validate(String phoneNumber){
-        Matcher matcher = phoneNumberPattern.matcher(phoneNumber);
-
-        return matcher.matches();
+    public PhoneNumberValidator(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
+    @Override
+    public boolean validate(String phoneNumber){
+        return !isEmpty(phoneNumber) && isValidPattern(phoneNumber) && isUnique(phoneNumber);
+    }
 
+    public boolean isEmpty(String phoneNumber){
+        if (phoneNumber == null || phoneNumber.trim().isEmpty()) {
+            throw new IllegalArgumentException("Phone Number cannot be empty!");
+        }
+        return false;
+    }
+
+    public boolean isValidPattern(String phoneNumber){
+        if(!phoneNumberPattern.matcher(phoneNumber).matches()){
+            throw new IllegalArgumentException("The pattern of the phone number is invalid!");
+        }
+        return true;
+    }
+
+    public boolean isUnique(String phoneNumber){
+        if(userRepository.existsByPhoneNumber(phoneNumber)){
+            throw new IllegalArgumentException("The phone number already exists!");
+        }
+        return true;
+    }
 }
