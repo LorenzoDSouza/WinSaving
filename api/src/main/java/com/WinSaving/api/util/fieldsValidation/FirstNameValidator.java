7cd266a2c6938ff215ec98b0096bcbee7dd5408d
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-25 13:35:12 -0300

    fix + refactor + feat: create the full validation for the first and last name field.
    Now both are components to be used with dependency injection in the UserRequestDTOValidation.

--- api/src/main/java/com/WinSaving/api/util/fieldsValidation/FirstNameValidator.java
+++ api/src/main/java/com/WinSaving/api/util/fieldsValidation/FirstNameValidator.java
@@ -1,19 +1,29 @@
 package com.WinSaving.api.util.fieldsValidation;
 
-import java.util.regex.Matcher;
+import org.springframework.stereotype.Component;
 import java.util.regex.Pattern;
 
-
+@Component
 public class FirstNameValidator implements Validator<String> {
 
     public static final Pattern firstNamePattern =
             Pattern.compile("^[A-Za-zÀ-ÿ]+(?:[ '-][A-Za-zÀ-ÿ]+)*$");
 
     @Override
-    public boolean validate(String input) {
-        Matcher matcher = firstNamePattern.matcher(input);
-
-        return matcher.matches();
+    public boolean validate(String firstName) {
+        return isNotEmpty(firstName) && isValidPattern(firstName);
     }
 
+    public boolean isNotEmpty(String firstName) {
+        if (firstName == null || firstName.trim().isEmpty()) {
+            throw new IllegalArgumentException("First Name cannot be empty!");
+        }
+        return true;
+    }
+    public boolean isValidPattern(String firstName){
+        if(!firstNamePattern.matcher(firstName).matches()){
+            throw new IllegalArgumentException("First name can only contain letters, spaces, apostrophes, and hyphens!");
+        }
+        return true;
+    }
 }
--- api/src/main/java/com/WinSaving/api/util/fieldsValidation/LastNameValidator.java
+++ api/src/main/java/com/WinSaving/api/util/fieldsValidation/LastNameValidator.java
@@ -1,8 +1,9 @@
 package com.WinSaving.api.util.fieldsValidation;
 
-import java.util.regex.Matcher;
+import org.springframework.stereotype.Component;
 import java.util.regex.Pattern;
 
+@Component
 public class LastNameValidator implements Validator<String> {
     public static final Pattern lastNamePattern =
             Pattern.compile("^[A-Za-zÀ-ÿ]+(?:[ '-]?[A-Za-zÀ-ÿ]+)*(?:[ '-]de[A-Za-zÀ-ÿ]+)?$");
@@ -10,8 +11,19 @@ public class LastNameValidator implements Validator<String> {
 
     @Override
     public boolean validate(String lastName) {
-        Matcher matcher = lastNamePattern.matcher(lastName);
+        return !isNotEmpty(lastName) && isValidPattern(lastName);
+    }
 
-        return matcher.matches();
+    public boolean isNotEmpty(String lastName) {
+        if (lastName == null || lastName.trim().isEmpty()) {
+            throw new IllegalArgumentException("Last Name cannot be empty!");
+        }
+        return true;
+    }
+    public boolean isValidPattern(String lastName){
+        if(!lastNamePattern.matcher(lastName).matches()){
+            throw new IllegalArgumentException("Last Name can only contain letters, spaces, apostrophes, and hyphens!");
+        }
+        return true;
     }
 }
