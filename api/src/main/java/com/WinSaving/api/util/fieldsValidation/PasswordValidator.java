16f1ebf5d12921400e679a1039780e2363ddb4df
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-25 13:47:21 -0300

    feat + refactor: create the complete logic to validate if a password from the UserRequestDTO is valid.
    Adds the method isNotEmpty (boolean) and combine it with a matcher for the password regex, that checks if the password has at least one uppercase letter, one lowercase, one number and one special character.
    Also turns the class into a component, to allow dependency injection.

--- api/src/main/java/com/WinSaving/api/util/fieldsValidation/PasswordValidator.java
+++ api/src/main/java/com/WinSaving/api/util/fieldsValidation/PasswordValidator.java
@@ -1,16 +1,33 @@
 package com.WinSaving.api.util.fieldsValidation;
 
-import java.util.regex.Matcher;
+import org.springframework.stereotype.Component;
 import java.util.regex.Pattern;
 
+@Component
 public class PasswordValidator implements Validator<String> {
     public static final Pattern passwordPattern =
-            Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\\\d)(?=.*[!@#$%^&*(),.?\\\":{}|<>])[A-Za-z\\\\d!@#$%^&*(),.?\\\":{}|<>]{8,}$");
+            Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*(),.?\":{}|<>])[A-Za-z\\d!@#$%^&*(),.?\":{}|<>]{8,}$");
 
     @Override
-    public boolean validate(String input) {
-        Matcher matcher = passwordPattern.matcher(input);
+    public boolean validate(String password) {
+        return isNotEmpty(password) && isValidPattern(password);
+    }
 
-        return matcher.matches();
+    public boolean isNotEmpty(String password) {
+        if(password == null || password.trim().isEmpty()){
+            throw new IllegalArgumentException("Password cannot be null or empty!");
+        }
+        return true;
+    }
+
+    public boolean isValidPattern(String password) {
+        if (!passwordPattern.matcher(password).matches()) {
+            throw new IllegalArgumentException("Invalid password!" +
+                    " It must contain at least one lowercase letter, " +
+                    "one uppercase letter, " +
+                    "one number and " +
+                    "one special character");
+        }
+        return true;
     }
 }
