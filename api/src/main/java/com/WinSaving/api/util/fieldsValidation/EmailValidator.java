81cbb2c5d70c7f1028503e6a4f1c9a2d0474617e
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-25 12:16:56 -0300

    feat + refactor: Add isUniqueEmail method to EmailValidator, checking both email uniqueness and pattern validation in the validate method.
    - Convert EmailValidator to a @Component for dependency injection in UserRequestDTOValidation.
    - Throw two exceptions: IllegalArgumentException for invalid email pattern and DataIntegrityViolationException for duplicate email.

--- api/src/main/java/com/WinSaving/api/util/fieldsValidation/EmailValidator.java
+++ api/src/main/java/com/WinSaving/api/util/fieldsValidation/EmailValidator.java
@@ -1,16 +1,40 @@
 package com.WinSaving.api.util.fieldsValidation;
 
+import com.WinSaving.api.repositories.UserRepository;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Component;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Component
 public class EmailValidator implements Validator<String> {
+    private final UserRepository userRepository;
+
     public static final Pattern emailPattern =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
+
+    public EmailValidator(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public boolean validate(String email) {
-        Matcher matcher = emailPattern.matcher(email);
+        return isValidPattern(email) && isUniqueEmail(email);
+    }
 
-        return matcher.matches();
+    public boolean isValidPattern(String email) {
+        if(!emailPattern.matcher(email).matches()){
+            throw new IllegalArgumentException("The pattern of the email is invalid!");
+        }
+        return true;
+    }
+
+    private boolean isUniqueEmail(String email) {
+        if (userRepository.existsByEmail(email)) {
+            throw new DataIntegrityViolationException("Email already registered!");
+        }
+        return true;
     }
 }
