0b79c7629947e9b3cbc173997b978a1c90292b54
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-25 15:11:05 -0300

    fix: remove the existsByPhoneNumber, because it will be allowed to have more than one account with the same phone number.

--- api/src/main/java/com/WinSaving/api/repositories/UserRepository.java
+++ api/src/main/java/com/WinSaving/api/repositories/UserRepository.java
@@ -7,6 +7,4 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByEmail(String email);
-
-    boolean existsByPhoneNumber(String phoneNumber);
 }
