3347adbcf75cc6ce28b572afed9a69700b00023a
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-26 16:07:49 -0300

    feat: add an extended message to the PhoneNumberValidator to help understand what's missing in the phone number input.

--- api/src/main/java/com/WinSaving/api/util/fieldsValidation/userFields/PhoneNumberValidator.java
+++ api/src/main/java/com/WinSaving/api/util/fieldsValidation/userFields/PhoneNumberValidator.java
@@ -1,12 +1,14 @@
 package com.WinSaving.api.util.fieldsValidation.userFields;
 
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
 import org.springframework.stereotype.Component;
 import java.util.regex.Pattern;
 
 @Component
 public class PhoneNumberValidator implements Validator<String> {
     //TODO: at first, only brazilian phone numbers will be accepted.
-
 
     private static final Pattern phoneNumberPattern =  Pattern.compile(
            "^(\\+55|55)\\s?(\\(\\s?\\d{2}\\s?\\)|\\d{2})\\s?9\\d{4}[\\s-]?\\d{4}$");
@@ -26,7 +28,8 @@ public class PhoneNumberValidator implements Validator<String> {
 
     public boolean isValidPattern(String phoneNumber){
         if(!phoneNumberPattern.matcher(phoneNumber).matches()){
-            throw new IllegalArgumentException("The pattern of the phone number is invalid!");
+            throw new IllegalArgumentException
+                    ("The pattern of the phone number is invalid! Rather ir's not a brazilian number or something is missing (remember to put the DDD and the Country Code).");
         }
         return true;
     }
