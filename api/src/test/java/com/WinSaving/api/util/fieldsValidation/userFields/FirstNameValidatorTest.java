92b2d1f986f186d88f60cb1555744b6305090a01
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-26 16:22:23 -0300

    test: create the test for the first name validator to check null or empty inputs (its throwing exceptions correctly).

--- api/src/test/java/com/WinSaving/api/util/fieldsValidation/userFields/FirstNameValidatorTest.java
+++ api/src/test/java/com/WinSaving/api/util/fieldsValidation/userFields/FirstNameValidatorTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class FirstNameValidatorTest {
@@ -22,5 +22,21 @@ public class FirstNameValidatorTest {
         assertTrue(firstNameValidator.isValidPattern("Érica"));
         assertTrue(firstNameValidator.isValidPattern("O'Connor"));
         assertTrue(firstNameValidator.isValidPattern("Jean-Luc"));
+    }
+
+    @Test
+    public void testIsEmpty_shouldThrowException_whenNameIsEmpty() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+            firstNameValidator.isNotEmpty("");
+        });
+        assertEquals(thrown.getMessage(), "First Name cannot be empty!");
+    }
+
+    @Test
+    public void testIsEmpty_shouldThrowException_whenNameIsNull() {
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+            firstNameValidator.isNotEmpty("");
+        });
+        assertEquals(thrown.getMessage(), "First Name cannot be empty!");
     }
 }
