21eb5f7049470233b5f2dae6b746a0b37a0c4d5f
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-26 11:08:44 -0300

    fix: corrects the monthly budget entity class.
    Add the map for the columns in the database.
    Add the min and max notation to payday.

--- api/src/main/java/com/WinSaving/api/domain/monthlyBudget/MonthlyBudget.java
+++ api/src/main/java/com/WinSaving/api/domain/monthlyBudget/MonthlyBudget.java
@@ -1,17 +1,15 @@
 package com.WinSaving.api.domain.monthlyBudget;
-
 import com.WinSaving.api.domain.expense.Expense;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.math.BigDecimal;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 
 @Entity
 @Table(name = "monthly_budgets")
@@ -20,22 +18,25 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MonthlyBudget {
+
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "UUID DEFAULT gen_random_uuid()")
     private UUID id;
 
-    @Column(nullable = false, precision = 10, scale = 2)
+    @Column(name = "used_amount", nullable = false, precision = 10, scale = 2, columnDefinition = "NUMERIC(10,2) DEFAULT 0.00")
     private BigDecimal usedAmount = BigDecimal.ZERO;
 
-    @Column(nullable = false, precision = 10, scale = 2)
+    @Column(name = "original_amount", nullable = false, precision = 10, scale = 2)
     private BigDecimal originalAmount = BigDecimal.ZERO;
 
-    private int payDay;
+    @Min(1) @Max(31)
+    @Column(name = "pay_day")
+    private Integer payDay;
 
+    @Column(name = "last_reset")
     private Date lastReset;
 
     @OneToMany(mappedBy = "monthlyBudget", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Expense> expenses = new ArrayList<>();
 }
-
-
