cb9a1942b7c5a7027dcb87bf116602bf5140d720
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-28 19:37:16 -0300

    feat: create the deleteByUsedId in the SavingGoalRepository.

--- api/src/main/java/com/WinSaving/api/repositories/SavingGoalRepository.java
+++ api/src/main/java/com/WinSaving/api/repositories/SavingGoalRepository.java
@@ -1,9 +1,14 @@
 package com.WinSaving.api.repositories;
 
 import com.WinSaving.api.domain.savingGoal.SavingGoal;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface SavingGoalRepository extends JpaRepository<SavingGoal, UUID> {
+    @Transactional
+    void deleteByUserId(UUID userId);
+
+
 }
