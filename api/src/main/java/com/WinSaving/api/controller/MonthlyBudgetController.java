a35b2840aba58dce1c919c87a7015dec717a7968
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-03-31 16:04:30 -0300

    feat: create the monthly budget controller.

---
+++ api/src/main/java/com/WinSaving/api/controller/MonthlyBudgetController.java
@@ -0,0 +1,9 @@
+package com.WinSaving.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/monthly-budgets")
+public class MonthlyBudgetController {
+}
