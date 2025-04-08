1025281e7279bd03ff80ac5250f4b383b6ef7f38
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-04-08 16:45:06 -0300

    feat: create the findByEmail in the UserRepository class.

--- api/src/main/java/com/WinSaving/api/repositories/UserRepository.java
+++ api/src/main/java/com/WinSaving/api/repositories/UserRepository.java
@@ -3,8 +3,11 @@ package com.WinSaving.api.repositories;
 import com.WinSaving.api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
