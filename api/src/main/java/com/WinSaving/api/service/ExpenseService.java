a2a3b723c9d38510f86daa17477a0b51e2294d0f
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-04-10 14:41:15 -0300

    feat: create the create expense method in the Expense Service, to be used in the Monthly budget service.

--- api/src/main/java/com/WinSaving/api/service/ExpenseService.java
+++ api/src/main/java/com/WinSaving/api/service/ExpenseService.java
@@ -1,10 +1,15 @@
 package com.WinSaving.api.service;
 
+import com.WinSaving.api.domain.expense.ExpenseRequestDTO;
+import com.WinSaving.api.domain.monthlyBudget.MonthlyBudget;
 import com.WinSaving.api.repositories.ExpenseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.WinSaving.api.domain.expense.Expense;
+
+import java.math.BigDecimal;
+import java.util.UUID;
 
 @Service
 public class ExpenseService {
@@ -17,9 +22,22 @@ public class ExpenseService {
     }
 
     @Transactional
-    public Expense createExpense() {
-       return null;
+    public Expense createExpense(ExpenseRequestDTO dto, MonthlyBudget monthlyBudget) {
+        //passar o id do monthly budget na request atraves do monthly budget controller
+        if (dto.value().compareTo(BigDecimal.ZERO) <= 0 ) {
+            throw new IllegalArgumentException("Value of the expense must be greater than zero");
+        }
+        Expense expense = new Expense();
+
+        expense.setValue(dto.value());
+        expense.setExpenseType(dto.type());
+        expense.setDescription(dto.description());
+        expense.setDate(dto.date());
+        expense.setMonthlyBudget(monthlyBudget);
+
+        return expenseRepository.save(expense);
     }
+
 
 
 
