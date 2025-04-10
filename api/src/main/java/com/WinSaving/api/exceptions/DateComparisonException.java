db54eeebbe879ea562979ad460b793626f899ee0
LorenzoDSouza <lkirstdesouza@gmail.com>
2025-04-10 15:51:58 -0300

    feat: create a date comparison exception to check dates.

---
+++ api/src/main/java/com/WinSaving/api/exceptions/DateComparisonException.java
@@ -0,0 +1,7 @@
+package com.WinSaving.api.exceptions;
+
+public class DateComparisonException extends RuntimeException {
+    public DateComparisonException(String message) {
+        super(message);
+    }
+}
